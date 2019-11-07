b13803c087b7d47ec7da1c5199ecc60a9b833dbe
ekisalman03 <55838966+ekisalman03@users.noreply.github.com>
2019-11-07 22:39:06 +0700

    Add files via upload

---
+++ PBO1-10118041-Latihan58-TambahKurang/src/pbo1/pkg10118041/latihan58/tambahkurang/PBO110118041Latihan58TambahKurang.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118041.latihan58.tambahkurang;
+
+/**
+ *
+ * @author 
+ * NAMA  : Eki salman sya'bani
+ * KELAS : IF 1
+ * NIM   : 10118041
+ * Deskripsi Program : Program ini untuk membuat tampilan 
+ *                     tambah kurang bilangan.
+ */
+public class PBO110118041Latihan58TambahKurang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        JumlahBilangan jBilangan = new JumlahBilangan();
+        jBilangan.tampilHasilJumlah();
+        
+        SelisihBilangan sBilangan = new SelisihBilangan();
+        sBilangan.tampilSelisih();
+    }
+    
+}
